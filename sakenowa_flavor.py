db25649092d89b844ffe06a5fb2c54d3d9f23247
3so <11akihiro10@gmail.com>
2020-12-13 14:19:33 +0900

    フロントサイドへの値受け渡し設定

--- sakenowa_flavor.py
+++ sakenowa_flavor.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-import numpy as np
 import apiget
-import json
+import flavor_chart as fc
 
 app = Flask(__name__)
 
@@ -11,12 +10,13 @@ def index():
     area_id = ""
     brewery_id = ""
     brand_id = ""
-    search_brands = ""
+    input_brand = ""
+    search_brands = apiget.search_brands(input_brand)
     areas = apiget.get_area()
     breweries = apiget.get_breweries(area_id)
     brands = apiget.get_brands(brewery_id)
     return render_template('index.html',
-                            areas=areas, breweries=breweries, brands=brands, area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands)
+                            areas=areas, breweries=breweries, brands=brands, area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands, input_brand=input_brand)
 
 # 絞り込み検索
 
@@ -26,14 +26,15 @@ def select_area():
     area_id = ""
     brewery_id = ""
     brand_id = ""
-    search_brands = ""
+    input_brand = ""
+    search_brands = apiget.search_brands(input_brand)
     if request.form['area'].isdecimal():
         area_id = int(request.form['area'])
     areas = apiget.get_area()
     breweries = apiget.get_breweries(area_id)
     brands = apiget.get_brands(brewery_id)
     return render_template('index.html',
-                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands)
+                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands, input_brand=input_brand)
 
 # 蔵元の選択
 @app.route('/select_brewery', methods=['GET', 'POST'])
@@ -41,7 +42,8 @@ def select_brewery():
     area_id = ""
     brewery_id = ""
     brand_id = ""
-    search_brands = ""
+    input_brand = ""
+    search_brands = apiget.search_brands(input_brand)
     if request.form['area'].isdecimal():
         area_id = int(request.form['area'])
     if request.form['brewery'].isdecimal():
@@ -50,7 +52,7 @@ def select_brewery():
     breweries = apiget.get_breweries(area_id)
     brands = apiget.get_brands(brewery_id)
     return render_template('index.html',
-                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands)
+                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands, input_brand=input_brand)
 
 # 銘柄の選択
 @app.route('/select_brand', methods=['GET', 'POST'])
@@ -58,7 +60,8 @@ def select_brand():
     area_id = ""
     brewery_id = ""
     brand_id = ""
-    search_brands = ""
+    input_brand = ""
+    search_brands = apiget.search_brands(input_brand)
     if request.form['area'].isdecimal():
         area_id = int(request.form['area'])
     if request.form['brewery'].isdecimal():
@@ -68,9 +71,11 @@ def select_brand():
     areas = apiget.get_area()
     breweries = apiget.get_breweries(area_id)
     brands = apiget.get_brands(brewery_id)
-    flavor = apiget.get_flavors(brand_id)
+    flavor = apiget.get_flavors(brand_id, brands)
+    flavor_chart = fc.get_flavor_chart(flavor)
+    flavor_tags = apiget.get_flavor_tags(brand_id)
     return render_template('index.html',
-                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands, flavor=flavor)
+                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, search_brands=search_brands, input_brand=input_brand, flavor=flavor, flavor_chart=flavor_chart, flavor_tags=flavor_tags)
 
 # 絞り込み検索終わり
 
@@ -102,9 +107,10 @@ def select_search_brand():
     areas = apiget.get_area()
     breweries = apiget.get_breweries(area_id)
     brands = apiget.get_brands(brewery_id)
-    flavor = apiget.get_flavors(selected_brand_id)
+    flavor = apiget.get_flavors(selected_brand_id, search_brands)
+    flavor_chart = fc.get_flavor_chart(flavor)
     return render_template('index.html',
-                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, selected_brand_id=selected_brand_id, search_brands=search_brands, input_brand=input_brand, flavor=flavor)
+                            areas=areas, breweries=breweries, brands=brands,area_id=area_id, brewery_id=brewery_id, brand_id=brand_id, selected_brand_id=selected_brand_id, search_brands=search_brands, input_brand=input_brand, flavor=flavor, flavor_chart=flavor_chart)
 
 
 if __name__ == '__main__':
